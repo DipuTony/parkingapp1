6220a583c6f57a5fe726128b54d2fd2549704fc9
Dipu Singh <dsingh197@gmail.com>
2023-01-06 13:33:36 +0530

    ask when back buttion exit

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button, View } from 'react-native';
+import { Button, View, BackHandler, Alert } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import LoginScreen from './src/Screens/LoginScreen';
@@ -7,6 +7,7 @@ import WebViewIndex from './src/Screens/WebViewIndex';
 import Sidebar from './src/Components/CustomDrawer';
 import { createStackNavigator } from '@react-navigation/stack';
 import { About } from './src/Screens/About'
+import {useEffect} from 'react-native'
 
 function HomeScreen({ navigation }) {
   return (
@@ -19,6 +20,7 @@ function HomeScreen({ navigation }) {
   );
 }
 
+
 function NotificationsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -30,6 +32,10 @@ function NotificationsScreen({ navigation }) {
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
+
+
+
+
 
 function MyStack() {
   return (
@@ -57,12 +63,48 @@ const AppDrawer = () => {
       />
       <Drawer.Screen name="Notifications" component={NotificationsScreen} />
       <Drawer.Screen name="Login" component={LoginScreen} />
-      <Drawer.Screen name="Web View" component={WebViewIndex}  options={{ headerShown: false }}/>
+      <Drawer.Screen name="Web View" component={WebViewIndex} options={{ headerShown: false }} />
     </Drawer.Navigator>
   )
 }
 
 export default function App() {
+
+  //====================== On Press back ask to exit
+
+React.useEffect(() => {
+  const backHandler = BackHandler.addEventListener(
+    'hardwareBackPress',
+    () => {
+      // Show a prompt asking the user if they want to exit the app
+      Alert.alert(
+        'Exit App',
+        'Are you sure you want to exit?',
+        [
+          {
+            text: 'Cancel',
+            onPress: () => { },
+            style: 'cancel'
+          },
+          {
+            text: 'OK',
+            onPress: () => BackHandler.exitApp()
+          }
+        ],
+        {
+          cancelable: false
+        }
+      );
+      return true;
+    }
+  );
+
+  return () => backHandler.remove();
+}, []);
+
+// ===================== End ask to ed
+
+
   return (
     <NavigationContainer>
       <AppDrawer />
